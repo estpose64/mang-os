7c1b95c13b27a762bc3e6978a549b35486af4933
EstPose64 <estpose64@gmail.com>
2026-03-06 16:23:41 -0300

    added vga_buffer module

--- src/main.rs
+++ src/main.rs
@@ -5,27 +5,20 @@
 
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello, World!";
+mod vga_buffer;
 
 // don't mangle the name of this function
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-	// this function is the entry point, since the linker looks for a function
-	// named `_start` by default
-	let vga_buffer = 0xb8000 as *mut u8;
+    // this function is the entry point, since the linker looks for a function
+    // named `_start` by default
+    vga_buffer::print_stuff();
 
-	for (i, &byte) in HELLO.iter().enumerate() {
-		unsafe {
-			*vga_buffer.offset(i as isize * 2) = byte;
-			*vga_buffer.offset(i as isize * 2 + 1) = 0x2;
-		}
-	}
-
-	loop {}
+    loop {}
 }
 
 /// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-	loop{}
+    loop {}
 }
